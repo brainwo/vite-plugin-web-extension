5997ec0e10654123b2b3eb7e4e682bcf9ec3787c
Brian Wo <45139213+brainwo@users.noreply.github.com>
2025-03-31 10:53:09 +0800

    feat: add bun

--- packages/create-vite-plugin-web-extension/src/types.ts
+++ packages/create-vite-plugin-web-extension/src/types.ts
@@ -1,5 +1,5 @@
-export const PACKAGE_MANAGERS = ["npm", "pnpm", "yarn"] as const;
-export type PackageManager = typeof PACKAGE_MANAGERS[number];
+export const PACKAGE_MANAGERS = ["npm", "pnpm", "yarn", "bun"] as const;
+export type PackageManager = (typeof PACKAGE_MANAGERS)[number];
 
 export interface InputProjectOptions {
   projectName?: string;
